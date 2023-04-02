a22f09a0683295c48faa8b6e24fa90b238739756
tristan <heytristaann@gmail.com>
2023-04-02 07:51:32 -0400

    cleaned up

--- equations.py
+++ equations.py
@@ -13,12 +13,9 @@ class Equations:
         # // Determine the power of the variable
         to_power = lambda p: f"x^{{{p}}}" if p > 1 else "x" if p == 1 else ""
         
-        # // Random number and the degree range
-        degree_range: range = range(degree - 1, -1, -1)
-
         # // Generate other variables beyond the first one
         other_variables: str = "".join([
-            f" {Equations.rand_op()} {randint(2, 9)}" + to_power(i) for i in degree_range
+            f" {Equations.rand_op()} {randint(2, 9)}" + to_power(i) for i in range(degree - 1, -1, -1)
         ])
 
         # // Build the equation
@@ -34,8 +31,4 @@ class Equations:
     # // Generate a random equation with brackets
     @staticmethod
     def add_brackets(equation: str, bracket_power: int) -> str:
-        # // Generate the power as a string
-        bracket_power_str: str = f"^{{{bracket_power}}}" if bracket_power > 1 else ""
-
-        # // Return the equation and power
-        return f"({equation})" + bracket_power_str
+        return f"({equation})" + f"^{{{bracket_power}}}" if bracket_power > 1 else ""
